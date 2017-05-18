baafc4b3b7327c57e76d52be2584bf51c4776221
tuckerwilliams <tuckerwilliams1@gmail.com>
2017-05-18 14:34:07 +0200

    commit #2

--- app/src/main/java/com/ait/tuckerwilliams/dontforgettosear/fragments/SettingsFragment.java
+++ app/src/main/java/com/ait/tuckerwilliams/dontforgettosear/fragments/SettingsFragment.java
@@ -1,36 +1,38 @@
 package com.ait.tuckerwilliams.dontforgettosear.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ait.tuckerwilliams.dontforgettosear.R;
+import com.github.machinarius.preferencefragment.PreferenceFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link EmptyFragment.OnEmptyFragmentListener} interface
+ * {@link SettingsFragment.OnSettingsFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link EmptyFragment#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EmptyFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Activity activity;
 
-    private OnEmptyFragmentListener mListener;
+    private OnSettingsFragmentInteractionListener mListener;
 
-    public EmptyFragment() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
@@ -39,25 +41,27 @@ public class EmptyFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmptyFragment.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EmptyFragment newInstance(String param1, String param2) {
-        EmptyFragment fragment = new EmptyFragment();
+    public static SettingsFragment newInstance(String param1) {
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //activity.setTheme(R.style.PreferenceThemeOverlay);
         super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        //addPreferencesFromResource(R.xml.app_preferences);
+
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String mParam1 = getArguments().getString(ARG_PARAM1);
         }
     }
 
@@ -65,21 +69,33 @@ public class EmptyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empty, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbarTop);
+        toolbar.setTitle("");
+        //toolbar.setDisplayShowTitleEnabled(false); //TODO-// FIXME: 5/10/17
+        TextView tbTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        tbTitle.setText(R.string.tb_settings_tile);
+        ((AppCompatActivity) activity).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onSettingsFragmentInteraction();
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnEmptyFragmentListener) {
-            mListener = (OnEmptyFragmentListener) context;
+        this.activity = getActivity();
+        if (context instanceof OnSettingsFragmentInteractionListener) {
+            mListener = (OnSettingsFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -102,8 +118,8 @@ public class EmptyFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnEmptyFragmentListener {
+    public interface OnSettingsFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onSettingsFragmentInteraction();
     }
 }
